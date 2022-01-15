48f161c4b3e8e6945bd00f858913e5742e5e502a
ちゅけ <77254130+erjkl1@users.noreply.github.com>
2022-01-15 17:04:04 +0900

    installed React(typescript)

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,8 +1,12 @@
-@extends('common.layout')
-
-@section('index')
-    <p>{{ $hello }}</p>
-    @foreach ($hello_array as $hello_word)
-        {{ $hello_word }}<br>
-    @endforeach
-@endsection
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+    <script src="{{ mix('js/app.js') }}"></script>
+</html>
--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -1,16 +1,8 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.ts('resources/ts/app.tsx', 'public/js')
+.sass('resources/sass/app.scss', 'public/css');
 
-mix.js('resources/js/app.js', 'public/js')
-    .react()
-    .sass('resources/sass/app.scss', 'public/css');
+if (mix.inProduction()){
+    mix.version();
+}
